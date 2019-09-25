05959322ef059aa97ef95442b048b1cc0160fd73
qingqingshu <645857537@qq.com>
2019-09-25 20:24:13 +0800

    math:136只出现一次的数字

---
+++ src/main/java/chuxiao/map/只出现一次的数字136.java
@@ -0,0 +1,28 @@
+package chuxiao.map;
+
+/**
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ * <p>
+ * 示例 1:
+ * 输入: [2,2,1]
+ * 输出: 1
+ * <p>
+ * 示例 2:
+ * 输入: [4,1,2,1,2]
+ * 输出: 4
+ */
+public class 只出现一次的数字136 {
+    public static void main(String[] args) {
+        int[] in = {4, 1, 2, 1, 2};
+        System.out.println(singleNumber(in));
+    }
+
+    //异或是比较不同
+    public static int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result = result ^ nums[i];
+        }
+        return result;
+    }
+}
