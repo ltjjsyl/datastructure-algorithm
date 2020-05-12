e9ad96fcae336647f510acee9817f5947c22f883
qingqingshu <645857537@qq.com>
2020-05-12 22:00:49 +0800

    add: linkedlist(get/remove)

--- src/main/java/chuxiao/datastructure/linear/list/v2/LinkedList.java
+++ src/main/java/chuxiao/datastructure/linear/list/v2/LinkedList.java
@@ -48,6 +48,97 @@ public class LinkedList<E> {
         add(size, e);
     }
 
+    public E get(int index) {
+
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Index must >=0 and < size.");
+        }
+
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        return cur.e;
+    }
+
+    public E getFirst() {
+        return get(0);
+    }
+
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    public boolean contains(E e) {
+
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.e.equals(e)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+
+        return false;
+    }
+
+    public void set(int index, E e) {
+
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Index must >=0 and < size.");
+        }
+
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        cur.e = e;
+    }
+
+    public E remove(int index) {
+
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Index must >=0 and < size.");
+        }
+
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+        }
+        Node retNode = prev.next;
+        prev.next = retNode.next;
+        retNode.next = null;
+        size--;
+
+        return retNode.e;
+    }
+
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder res = new StringBuilder();
+        res.append("LinkedList ");
+//        Node cur = dummyHead.next;
+//        while (cur != null) {
+//            res.append(cur.e).append("->");
+//            cur = cur.next;
+//        }
+        for (Node cur = dummyHead.next; cur != null; cur = cur.next) {
+            res.append(cur.e).append("->");
+        }
+        res.append("NULL");
+
+        return res.toString();
+    }
+
     private class Node {
         public E e;
         public Node next;
